12da9f9fa2ec8f8a52138f11b670a32db84f9692
Sophie Mann <sklm@live.co.uk>
2017-09-26 17:07:34 +0100

    Add home view

--- fruitapp/urls.py
+++ fruitapp/urls.py
@@ -1,4 +1,4 @@
-"""hellowebapp URL Configuration
+"""fruitapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from collection import views
+
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
