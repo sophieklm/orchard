48eff3acc2975b7595dc6dc76b4a0d56c91ee3a5
Sophie Mann <sklm@live.co.uk>
2017-10-03 13:11:35 +0100

    Add pages and routes

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+thing = "Thing Name"
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'thing': thing,
+})
