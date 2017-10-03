ee76507cd41ff6b5cb5bfd45d345deed78fdb387
Sophie Mann <sklm@live.co.uk>
2017-10-03 18:37:04 +0100

    Add type model

--- collection/admin.py
+++ collection/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
+from django import forms
 
 # Register your models here.
-from collection.models import Fruit
+from collection.models import Type, Fruit
+
+class FruitForm(forms.ModelForm):
+    FRUITS=Type.objects.values('fruit').distinct()
+    TYPES=Type.objects.values('type').distinct()
+    fruit = forms.ModelChoiceField(queryset=FRUITS, to_field_name="fruit")
+    type = forms.ModelChoiceField(queryset=TYPES, to_field_name="type")
+    def __init__(self, *args, **kwargs):
+        fruitid = kwargs.pop('fruit', None)
+        super(FruitForm, self).__init__(*args, **kwargs)
+
+        if fruitid:
+            self.fields['type'].queryset = Type.objects.filter(fruit=fruitid)
+    class Meta:
+        model = Fruit
+        fields = ['variety', 'fruit', 'type', 'harvest', 'taste', 'description', 'slug']
+        widgets = {
+            'variety': forms.TextInput(attrs={'placeholder': 'Barland'}),
+            'harvest': forms.TextInput(attrs={'placeholder': 'September-October'}),
+            'taste': forms.TextInput(attrs={'placeholder': 'Sweet'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Enter description here'}),
+            'slug': forms.TextInput(attrs={'placeholder': 'Autopopulated URL'}),
+        }
 
 class FruitAdmin(admin.ModelAdmin):
     model = Fruit
     list_display = ('variety', 'fruit', 'type', 'harvest', 'taste', 'description',)
     prepopulated_fields = { 'slug': ('variety',)}
+    form = FruitForm
 
+class TypeAdmin(admin.ModelAdmin):
+    model = Type
+    list_display = ('fruit', 'type')
+
+admin.site.register(Type, TypeAdmin)
 admin.site.register(Fruit, FruitAdmin)
--- collection/models.py
+++ collection/models.py
@@ -3,11 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Fruit(models.Model):
-    variety = models.CharField(max_length=255)
+class Type(models.Model):
     type = models.CharField(max_length=255)
     fruit = models.CharField(max_length=255)
-    harvest = models.CharField(max_length=255)
-    taste = models.CharField(max_length=255)
-    description = models.TextField()
+
+    def __unicode__(self):
+        return u'{0}'.format(self.type)
+
+class Fruit(models.Model):
+    variety = models.CharField(max_length=255, unique=True)
+    type = models.CharField(max_length=255)
+    fruit = models.CharField(max_length=255)
+    harvest = models.CharField(max_length=255, blank=True)
+    taste = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
+
