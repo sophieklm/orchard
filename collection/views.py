465361ec6bcdaf4abe01b60f52afeff731f91720
Sophie Mann <sklm@live.co.uk>
2017-10-04 19:17:17 +0100

    Update display

--- collection/views.py
+++ collection/views.py
@@ -4,6 +4,7 @@ from collection.models import Tree
 # Create your views here.
 def index(request):
     trees = Tree.objects.all()
+
     return render(request, 'index.html', {
         'trees': trees,
 })
--- fruitapp/settings.py
+++ fruitapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'multiselectfield',
+    'bootstrap_toolkit'
 ]
 
 MIDDLEWARE_CLASSES = [
