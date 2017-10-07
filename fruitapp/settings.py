5e45a8e0a4870b775a2d4504ca1b4730d54d64bc
Sophie Mann <sklm@live.co.uk>
2017-10-07 09:37:55 +0100

    Set debug false

--- fruitapp/settings.py
+++ fruitapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0xa(*n0=5rx!b^l_z=^q)d2b*2(k*yb$8n7(@j84q(s4^i*9oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- fruitapp/wsgi.py
+++ fruitapp/wsgi.py
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fruitapp.settings")
 
 application = get_wsgi_application()
+
+# import os
+# import sys
+
+# path = os.path.expanduser('~/orchard')
+# if path not in sys.path:
+#     sys.path.append(path)
+
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'fruitapp.settings'
+
+# from django.core.wsgi import get_wsgi_application
+# from django.contrib.staticfiles.handlers import StaticFilesHandler
+# application = StaticFilesHandler(get_wsgi_application())
