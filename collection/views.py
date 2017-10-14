d02a1671f2b5655840a27bd47056a0f5fcd554e8
Sophie Mann <sklm@live.co.uk>
2017-10-14 16:55:02 +0100

    Update templates

--- collection/views.py
+++ collection/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from collection.models import Tree
+import operator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
     trees = Tree.objects.all()
     varieties = Tree.objects.values('variety').distinct()
     months = Tree.objects.values('harvest').distinct()
+    eater = reduce(operator.and_, (Q(type__contains=item) for item in [u'Eater']))
+    eaters = Tree.objects.filter(eater)
+    juicer = reduce(operator.and_, (Q(type__contains=item) for item in [u'Juicer']))
+    juicers = Tree.objects.filter(juicer)
+    brewer = reduce(operator.and_, (Q(type__contains=item) for item in [[u'Perry'], [u'Cider']]))
+    brewers = Tree.objects.filter(brewer)
+    cooker = reduce(operator.and_, (Q(type__contains=item) for item in [u'Cooker']))
+    cookers = Tree.objects.filter(cooker)
 
     return render(request, 'index.html', {
         'trees': trees,
         'varieties': varieties,
-        'months': months
+        'months': months,
+        'eaters': eaters,
+        'juicers': juicers,
+        'brewers': brewers,
+        'cookers': cookers,
 })
 
 def fruit(request):
